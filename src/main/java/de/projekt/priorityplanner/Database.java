51a863adff33c2ba6a4610d1c74d06deafe283c0
mia <8mahncke@informatik.uni-hamburg.de>
2021-03-10 16:37:14 +0100

    added comments and modified unique name method

--- src/main/java/de/projekt/priorityplanner/Database.java
+++ src/main/java/de/projekt/priorityplanner/Database.java
@@ -2,41 +2,53 @@ package de.projekt.priorityplanner;
 
 
 import de.projekt.priorityplanner.model.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
-// TODO: replace with actual Database
-// entity Room (or Session):  id, [usernames], adminSessionId
-// entity UserStories: id, roomId, String, (maybe username too)
+/**
+ * Die statische Datanbank-Klasse ersetzt die Datenbank-Anbindung/Datenbank und verwaltet die Räume.
+ * Die Klasse soll in Zukunft durch eine Datenbank ersetzt werden die aber die zugrundeliegende Logik beibehält
+ * @author Mia Mahncke, Nedim Seroka
+ * @date 14.03.2021
+ */
 public class Database {
 
     static Map<Integer, Room> rooms1 = new ConcurrentHashMap();
-
-    static Map<Integer, String> admins = new HashMap();
-
     static Map<Integer, Integer> counters = new ConcurrentHashMap();
-
     static int n = 0;
 
+    /**
+     * erstellt einen Raum und fügt ihn der Liste hinzu
+     * @param roomName Name des Raums
+     * @return RaumID
+     */
     static public synchronized int addRoom(String roomName) {
         n++;
         //rooms.put(n, new LinkedList<String>());
-        Room room = new Room(n, roomName,  Collections.synchronizedList(new ArrayList<Feature>()), Collections.synchronizedList(new ArrayList<User>()), null, null);
+        Room room = new Room(n, roomName, Collections.synchronizedList(new ArrayList<Feature>()), Collections.synchronizedList(new ArrayList<User>()), null, null);
         rooms1.put(n, room);
         //userStories.put(n, new LinkedList<UserStory>());
-        admins.put(n, null);
         counters.put(n, 0);
         return n;
     }
 
+    /**
+     * Fügt einem feature in einem best. Raum eine Bewertung hinzu
+     * @param vote bewetung
+     * @param roomId raum in dem bewertet wurde
+     * @param featureId feature, das bewertet wurde
+     */
     static public synchronized void addVote(Vote vote, int roomId, int featureId) {
         Room room = rooms1.get(roomId);
         room.getFeatureById(featureId).addVote(vote);
     }
 
+    /**
+     * testet ob alle user das aktive Feature bewertet haben
+     * @param roomId RaumID in dem das Feature exsistiert
+     * @return ob alle user eine Bewertung abgegeben haben
+     */
     static public Boolean allVoted(int roomId) {
         Room room = rooms1.get(roomId);
         if (room.getNumberOfVotes() == counters.get(roomId)) {
@@ -45,53 +57,56 @@ public class Database {
 
     }
 
-    static public synchronized void setAdmin(int i, String sessionId) {
-        admins.replace(i, sessionId);
-    }
-
-    static public synchronized boolean adminNull(int i) {
-        return admins.get(i) == null;
-    }
-
-    static public boolean adminEquals(int i, String sessionId) {
-        return admins.get(i).equals(sessionId);
-    }
-
-    static public synchronized void addUsername(int i, String username, String rolle) {
+    /**
+     * fügt einen user hinzu
+     * @param roomId RaumID des Raums in den der user kommt
+     * @param username Name des neuen Users
+     * @param rolle Rolle des neuen Users
+     */
+    static public synchronized void addUser(int roomId, String username, String rolle) {
         //rooms.get(i).add(username);
-        rooms1.get(i).addUser(new User(username, rolle));
-        int v = counters.get(i);
-        counters.replace(i, ++v);
+        rooms1.get(roomId).addUser(new User(username, rolle));
+        int v = counters.get(roomId);
+        counters.replace(roomId, ++v);
     }
 
-    static public synchronized int reduceCounter(int i) {
-        int v = counters.get(i);
-        counters.replace(i, --v);
+    /**
+     * Reduziert den Counter, wenn ein User den Raum, verlässt
+     * @param roomId Raum aus dem der User ausgetreten ist
+     * @return Anzahl der verbleibenden User
+     */
+    static public synchronized int reduceCounter(int roomId) {
+        int v = counters.get(roomId);
+        counters.replace(roomId, --v);
         return v;
     }
 
-    static public synchronized List getUsernames(int i) {
-        //return rooms.get(i);
-        Room room = rooms1.get(i);
+    /**
+     * Gibt die Liste der Usernames aus für einen Raum
+     * @param roomId raumID aus der die Usernamen kommen
+     * @return Liste der Usernamen
+     */
+    static public synchronized List getUsernames(int roomId) {
+        Room room = rooms1.get(roomId);
         return room.getOnlyUserNames();
     }
 
+    /**
+     * Gibt an, ob ein Raum exsistiert
+     * @param i raumId
+     * @return ob der Raum mit raumId i exsistiert
+     */
     static public synchronized boolean containsRoom(int i) {
-        //return rooms.get(i) != null;
         return rooms1.get(i) != null;
     }
 
-    static public List<Feature> getFeatures(int roomId) {
+    /**
+     * gibt den Raum mit der RaumID roomId zurück
+     * @param roomId
+     * @return Raum mit RaumID roomId
+     */
+    static public Room getRoom(int roomId) {
         Room room = rooms1.get(roomId);
-        if (room != null) {
-            return room.getFeatures();
-        } else {
-            return null;
-        }
-    }
-
-    static public Room getRoom(int i) {
-        Room room = rooms1.get(i);
         if (room != null) {
             return room;
         } else {
@@ -100,19 +115,33 @@ public class Database {
         return null; //TODO remove this
     }
 
-    static public synchronized void addUserStory(int roomId, Feature feature) {
+    /**
+     * Fügt ein Feature in einem Raum hinzu
+     * @param roomId RaumID von dem Raum in dem das Feature hinzugefügt wird
+     * @param feature Feature das hinzugefügt wird
+     */
+    static public synchronized void addFeature(int roomId, Feature feature) {
         //userStories.get(roomId).add(UserStory);
         Room room = rooms1.get(roomId);
         room.addFeature(feature);
-
     }
 
-    static public List<Feature> getUserStories(int roomId) {
-        //return userStories.get(roomId);
+    /**
+     * gibt die Features aus einem Raum wieder
+     * @param roomId RaumId von Raum aus dem die Features wiedergegeben werden
+     * @return Features
+     */
+    static public List<Feature> getFeatures(int roomId) {
         Room room = rooms1.get(roomId);
         return room.getFeatures();
     }
 
+    /**
+     * entfertn einen User
+     * @param roomId raumID von Raum aus dem User entfernt wird
+     * @param username Name des User der entfernt wird
+     * @return Liste der verbleibenden User
+     */
     public static synchronized List<String> removeUser(int roomId, String username) {
         Room room = rooms1.get(roomId);
         room.removeUser(username);
@@ -125,29 +154,58 @@ public class Database {
         return room.getOnlyUserNames();
     }
 
+    /**
+     * Setzt das feature mit FeatureID featureid aktiv
+     * @param roomId ID von dem Raum in dem das Feature aktiv gesetzt wird
+     * @param featureId ID des Features das aktiv gesetzt wird
+     * @return das aktive Feature
+     */
     public static Feature selectFeature(int roomId, int featureId) {
         Room room = rooms1.get(roomId);
         return room.selectFeature(featureId);
     }
 
+    /**
+     * Erzeugt einen Namen der noch nicht im Raum vorkommt
+     * @param name Name der schon im Raum vorkommt
+     * @param roomId ID von dem Raum
+     * @return neuer Name
+     */
     public static synchronized String generateUniqueName(String name, int roomId) {
         String s = name;
-        while (getUsernames(roomId).contains(s)) {
-            s = s + "1";
+        int i = 1;
+        while (getUsernames(roomId).contains(s + i)) {
+            i++;
         }
-        return s;
+        return s+i;
     }
 
+    /**
+     * Löscht ein Feature
+     * @param roomId ID des Raums in dem das Feature gelöscht wird
+     * @param id ID des Features das gelöscht wird
+     */
     public static synchronized void deleteFeature(int roomId, int id) {
         Room room = rooms1.get(roomId);
         room.deleteFeature(id);
     }
 
+    /**
+     * gibt ein Feature aus einem Raum wieder
+     * @param roomId ID des Raums
+     * @param featureId ID des Features
+     * @return passendes Feature
+     */
     public static Feature getFeature(int roomId, int featureId) {
         Room room = rooms1.get(roomId);
         return room.getFeatureById(featureId);
     }
 
+    /**
+     * gibt das nächste, noch nicht bewertete Feature aus einem Raum wieder
+     * @param roomId ID des Raums
+     * @return nächstes unbewertetes Feature
+     */
     public static Feature getNextFeature(int roomId) {
         Room room = rooms1.get(roomId);
         return room.getNextFeature();
